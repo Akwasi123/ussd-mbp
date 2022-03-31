f8011a9580c2eadd2fcc79fbc0f432d9cd479fa0
Akwasi Asante-Krobea <49639685+Akwasi123@users.noreply.github.com>
2022-03-31 15:11:56 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-echo "Hello world";
+$msisdn = $_GET['msisdn'];
+$sequence_ID = $_GET['sequenceID'];
+$data = $_GET['data'];
+$type = 1;
+
+
+$mereply = "Hello David Sampah";
+echo $mereply . '|' . $msisdn . '|' . $sequence_id . '|' . $type;
