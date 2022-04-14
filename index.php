235e9daff1b5f91eae69c522e4b2372357aa42f1
Akwasi Asante-Krobea <49639685+Akwasi123@users.noreply.github.com>
2022-04-14 15:19:10 +0000

    update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php $msisdn = $_GET['msisdn'];
 $sequence_ID = $_GET['sequenceID'];
 $data
-    = $_GET['data'];
+    = $_POST['data'];
 $type = 0;
 $mereply = "Welcome To Ashesi Mobile App 2022\n
     1. choose a movie\n     2. select a music\n     3. thank you\n";
