4359ea099b7e252218fd3a4f8c28dce1e1516d49
Akwasi Asante-Krobea <49639685+Akwasi123@users.noreply.github.com>
2022-04-14 15:05:54 +0000

    update

--- index.php
+++ index.php
@@ -3,8 +3,13 @@
 $sequence_ID = $_GET['sequenceID'];
 $data
     = $_GET['data'];
-$type = 1;
+$type = 0;
 $mereply = "Welcome To Ashesi Mobile App 2022\n
     1. choose a movie\n     2. select a music\n     3. thank you\n";
 echo
-$mereply . '|' . $msisdn . '|' . $sequence_id . '|' . $type; ?>
+$mereply . '|' . $msisdn . '|' . $sequence_id . '|' . $type;
+
+
+if ($mereply == 1) {
+    echo "You selected a movie";
+}
